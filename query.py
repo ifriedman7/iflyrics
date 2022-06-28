0af1c379fcccec77c1305f4af1ecd2f39e8dd40b
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-06-28 21:11:42 +0300

    Update query.py

--- query.py
+++ query.py
@@ -31,26 +31,32 @@ def main(args):
     myapikey = 'b593f7edbd42fe8106a2bb85ad4f8f91'
     url = urllib.parse.urljoin('https://api.musixmatch.com/ws/1.1/track.search', '?q_lyrics=' + qstring + '&f_lyrics_language=en&apikey=' + myapikey)
     try:
+#       Using urllib module. Not best for json parsing ###    
 #        response = urllib.request.urlopen(url)
 #        headers = str(response.info())
 #        textData = str(response.read())
-        
-        
+####    
         Response = requests.get(url)
         Response.raise_for_status
         jsonResponse = Response.json()
         strJsonResponse = str(jsonResponse)
-    
+#   Not Using urllib module ###    
 #    except urllib.error.HTTPError as err:
 #        print(err.code)
+
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
         print(f'Other error occurred: {err}')        
         
+    for key, value in jsonResponse.items():
+        print(jsonResponse["track_name"]
+        
     
     with open('response.txt', 'w', encoding="utf-8") as f:
         f.write(strJsonResponse)
+        
+#   Not Using urllib module ###        
 #        f.write(headers)
 #        f.write(textData)
         
