14db4a5add735f8352cdf8b30b0ee298ff6e09fb
ifriedman7 <97619375+ifriedman7@users.noreply.github.com>
2022-06-29 00:09:33 +0300

    Update query.py

--- query.py
+++ query.py
@@ -56,7 +56,7 @@ def main(args):
 #                for track in track_list[0]:
 #                    print (track['track_name'])
                     
-    for track in jsonResponse['message']['body']['track_list'].get('track'):
+    for track in jsonResponse['message']['body']['track_list'].fromkeys('track'):
         print (track.get("track_name"))
                 
 #        print(jsonResponse.get("track_name"))
